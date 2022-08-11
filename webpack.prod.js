53d2a505d7bab836d9dc07aba29a9d008ab0cdb0
Joe Eli McIlvain <joe.eli.mac@gmail.com>
2022-08-11 16:35:16 -0700

    Add `deploy` CI workflow to deploy to GitHub Pages.

---
+++ webpack.prod.js
@@ -0,0 +1,24 @@
+const webpack = require("webpack")
+const merge = require("webpack-merge")
+const common = require("./webpack.config.js")
+const TerserPlugin = require("terser-webpack-plugin")
+
+module.exports = merge.merge(common, {
+  mode: "production",
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          mangle: true,
+        },
+      }),
+    ],
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      __IN_DEBUG__: JSON.stringify(false),
+      __IN_WORKER__: JSON.stringify(false),
+    }),
+  ],
+})
